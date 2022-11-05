38d60643df78639064b859fbf7467e6df42f61df
Mylka18 <1356729@senati.pe>
2022-11-05 18:17:58 -0500

    agregue link y te manda de frente

--- resources/views/navegable.blade.php
+++ resources/views/navegable.blade.php
@@ -17,31 +17,28 @@
     <body>
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="https://campusonline.senati.edu.pe/">SENATI</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="https://sinfo.senati.edu.pe/auth">SINFO</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="https://www.senati.edu.pe/alumnos">ALUMNOS</a>
             </li>
             <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
+            <a class="nav-link dropdown-toggle" href="https://www.senati.edu.pe/egresados" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                EGRESADOS
             </a>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="https://www.senati.edu.pe/egresados/bolsa-de-trabajo">BOLSA DE TRABAJO</a></li>
+                <li><a class="dropdown-item" href="https://www.senati.edu.pe/egresados/noticias-y-eventos">NOTICIAS Y EVENTOS</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="https://www.senati.edu.pe/buscar/tipo/master">MASTERS</a></li>
             </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
             </li>
         </ul>
         <form class="d-flex" role="search">
@@ -51,5 +48,6 @@
         </div>
     </div>
     </nav>
+
     </body>
 </html>
